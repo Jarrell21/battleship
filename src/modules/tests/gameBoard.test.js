f376eea3fde594fd0cca4e6a9f79270a2467a4db
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-20 00:08:57 +0800

    Add more tests

--- src/modules/tests/gameBoard.test.js
+++ src/modules/tests/gameBoard.test.js
@@ -1,76 +1,170 @@
 import GameBoard from '../gameBoard';
 import Ship from '../shipFactory';
 
-test('place ship at specific coordinates', () => {
+test('place ship horizontally', () => {
+  const board = GameBoard();
+  const patrolBoat = Ship(2);
+
+  board.placeShip(patrolBoat, 0);
+
+  expect(board.getBoard()[0]).toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: patrolBoat,
+  });
+  expect(board.getBoard()[1]).toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: patrolBoat,
+  });
+});
+
+test('place ship vertically', () => {
+  const board = GameBoard();
+  const patrolBoat = Ship(2);
+
+  board.placeShip(patrolBoat, 0, true);
+
+  expect(board.getBoard()[0]).toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: patrolBoat,
+  });
+  expect(board.getBoard()[10]).toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: patrolBoat,
+  });
+});
+
+test('Ships cannot be placed if they dont fit the board horizontally', () => {
   const board = GameBoard();
   const carrier = Ship(5);
 
-  board.placeShip(carrier, 0);
+  board.placeShip(carrier, 6, false);
 
-  expect(board.getBoard()).toContainEqual({
+  expect(board.getBoard()[6]).not.toStrictEqual({
     hasShip: true,
     isShot: false,
     ship: carrier,
   });
 });
 
-test('receive attack and hit', () => {
+test('Ships cannot be placed if they dont fit the board vertically', () => {
   const board = GameBoard();
   const carrier = Ship(5);
 
-  board.placeShip(carrier, 1);
+  board.placeShip(carrier, 60, true);
+
+  expect(board.getBoard()[60]).not.toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: carrier,
+  });
+});
+
+test('Ships cannot be placed if coordinate is taken by another ship', () => {
+  const board = GameBoard();
+  const carrier = Ship(5);
+  const patrolBoat = Ship(2);
+
+  board.placeShip(carrier, 0, false);
+  board.placeShip(patrolBoat, 1, false);
+
+  expect(board.getBoard()[1]).toStrictEqual({
+    hasShip: true,
+    isShot: false,
+    ship: carrier,
+  });
+});
+
+test('Random place ships', () => {
+  const board = GameBoard();
+
+  board.randomPlaceShips();
+
+  expect(board.getEmptyCoordinates()).toBe(83);
+});
+
+test('receive attack and hit', () => {
+  const board = GameBoard();
+  const patrolBoat = Ship(2);
+
+  board.placeShip(patrolBoat, 1);
+  board.receiveAttack(0);
   board.receiveAttack(1);
   board.receiveAttack(2);
   board.receiveAttack(3);
-  board.receiveAttack(4);
-  board.receiveAttack(5);
-  board.receiveAttack(6);
 
-  expect(carrier.getNumOfHits()).toBe(5);
+  expect(patrolBoat.getNumOfHits()).toBe(2);
 });
 
 test('receive attack and miss', () => {
   const board = GameBoard();
-  const carrier = Ship(5);
+  const patrolBoat = Ship(2);
 
-  board.placeShip(carrier, 0);
+  board.placeShip(patrolBoat, 0);
   board.receiveAttack(0);
   board.receiveAttack(1);
-  board.receiveAttack(7);
-  board.receiveAttack(8);
+  board.receiveAttack(2);
 
-  expect(carrier.getNumOfHits()).toBe(2);
+  expect(patrolBoat.getNumOfHits()).toBe(2);
   expect(board.getBoard()[0].isShot).toBeTruthy();
   expect(board.getBoard()[1].isShot).toBeTruthy();
-  expect(board.getBoard()[7].isShot).toBeTruthy();
-  expect(board.getBoard()[8].isShot).toBeTruthy();
+  expect(board.getBoard()[2].isShot).toBeTruthy();
 });
 
-test('All of the ships have been sunk', () => {
+test('Reset board', () => {
+  const board = GameBoard();
+  const patrolBoat = Ship(2);
+
+  board.placeShip(patrolBoat, 0);
+  board.resetBoard();
+
+  expect(board.getBoard()).not.toContainEqual({
+    hasShip: true,
+    isShot: false,
+    ship: patrolBoat,
+  });
+});
+
+test('All of the ships have sunk', () => {
   const board = GameBoard();
   const carrier = Ship(5);
   const battleship = Ship(4);
-  const cruiser = Ship(3);
+  const destroyer = Ship(3);
   const submarine = Ship(3);
-  const destroyer = Ship(2);
+  const patrolBoat = Ship(2);
 
-  board.placeShip(carrier, 0);
-  board.placeShip(battleship, 7);
-  board.placeShip(cruiser, 12);
-  board.placeShip(submarine, 16);
-  board.placeShip(destroyer, 21);
+  board.placeShip(carrier, 0, false);
+  board.placeShip(battleship, 10, false);
+  board.placeShip(destroyer, 20, false);
+  board.placeShip(submarine, 30, false);
+  board.placeShip(patrolBoat, 40, false);
 
-  function receiveAttackLoop(index, length) {
+  // function receiveAttackLoopVert(index, length) {
+  //   for (let i = 0; i < length; i += 1) {
+  //     board.receiveAttack(index + i * 10);
+  //   }
+  // }
+
+  function receiveAttackLoopHori(index, length) {
     for (let i = 0; i < length; i += 1) {
       board.receiveAttack(index + i);
     }
   }
 
-  receiveAttackLoop(0, 5);
-  receiveAttackLoop(7, 4);
-  receiveAttackLoop(12, 3);
-  receiveAttackLoop(16, 3);
-  receiveAttackLoop(21, 2);
+  // receiveAttackLoopVert(0, 5);
+  // receiveAttackLoopVert(1, 4);
+  // receiveAttackLoopVert(2, 3);
+  // receiveAttackLoopVert(3, 3);
+  // receiveAttackLoopVert(4, 2);
 
-  expect(board.allShipsAreSunk()).toBeTruthy();
+  receiveAttackLoopHori(0, 5);
+  receiveAttackLoopHori(10, 4);
+  receiveAttackLoopHori(20, 3);
+  receiveAttackLoopHori(30, 3);
+  receiveAttackLoopHori(40, 2);
+
+  expect(board.allShipsHaveSunk()).toBeTruthy();
 });
