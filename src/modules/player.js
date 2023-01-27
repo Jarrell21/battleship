be1fe8db74cafe3b2265bf30cc638c6838129b37
Jarrell Mikko Romero <88756582+Jarrell21@users.noreply.github.com>
2023-01-27 11:36:08 +0800

    Add computerPlay function

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,6 +1,9 @@
 const Player = (name) => {
   const getName = () => name;
-  const shotCoordinates = [];
+  let shotCoordinates = [];
+  let firstShotHit = null;
+  let lastShot = null;
+  let lastShotDirection = null;
 
   const attack = (gameBoard, index) => {
     if (isAlreadyShot(index)) return false;
@@ -13,11 +16,7 @@ const Player = (name) => {
   const randomAttack = (gameBoard) => {
     if (shotCoordinates.length === 100) return;
 
-    let coordinate = Math.floor(Math.random() * 100);
-
-    while (isAlreadyShot(coordinate)) {
-      coordinate = Math.floor(Math.random() * 100);
-    }
+    const coordinate = computerPlay(gameBoard);
 
     shotCoordinates.push(coordinate);
     gameBoard.receiveAttack(coordinate);
@@ -25,7 +24,170 @@ const Player = (name) => {
 
   const isAlreadyShot = (index) => shotCoordinates.includes(index);
 
-  return { attack, randomAttack, getName };
+  // Algorithm for making the computer attacks a little smarter
+  const computerPlay = (gameBoard) => {
+    let coordinate = Math.floor(Math.random() * 100);
+
+    while (isAlreadyShot(coordinate)) {
+      coordinate = Math.floor(Math.random() * 100);
+    }
+
+    if (firstShotHit !== null && lastShot === null) {
+      if (lastShotDirection === 'up') {
+        if (
+          isAttackLogical(firstShotHit, 'down') &&
+          !isAlreadyShot(firstShotHit + 10)
+        ) {
+          lastShotDirection = 'down';
+          coordinate = firstShotHit + 10;
+        } else {
+          firstShotHit = null;
+          lastShotDirection = '';
+        }
+      } else if (lastShotDirection === 'down') {
+        if (
+          isAttackLogical(firstShotHit, 'up') &&
+          !isAlreadyShot(firstShotHit - 10)
+        ) {
+          lastShotDirection = 'up';
+          coordinate = firstShotHit - 10;
+        } else {
+          firstShotHit = null;
+          lastShotDirection = '';
+        }
+      } else if (
+        isAttackLogical(firstShotHit, 'right') &&
+        !isAlreadyShot(firstShotHit + 1)
+      ) {
+        lastShotDirection = 'right';
+        coordinate = firstShotHit + 1;
+      } else if (
+        isAttackLogical(firstShotHit, 'left') &&
+        !isAlreadyShot(firstShotHit - 1)
+      ) {
+        lastShotDirection = 'left';
+        coordinate = firstShotHit - 1;
+      } else if (
+        isAttackLogical(firstShotHit, 'up') &&
+        !isAlreadyShot(firstShotHit - 10)
+      ) {
+        lastShotDirection = 'up';
+        coordinate = firstShotHit - 10;
+      } else if (
+        isAttackLogical(firstShotHit, 'down') &&
+        !isAlreadyShot(firstShotHit + 10)
+      ) {
+        lastShotDirection = 'down';
+        coordinate = firstShotHit + 10;
+      } else {
+        firstShotHit = null;
+        lastShotDirection = '';
+      }
+    } else if (lastShot !== null) {
+      if (lastShotDirection === 'up') {
+        if (isAttackLogical(lastShot, 'up') && !isAlreadyShot(lastShot - 10)) {
+          lastShotDirection = 'up';
+          coordinate = lastShot - 10;
+        } else if (
+          isAttackLogical(firstShotHit, 'down') &&
+          !isAlreadyShot(firstShotHit + 10)
+        ) {
+          lastShotDirection = 'down';
+          coordinate = firstShotHit + 10;
+        }
+      } else if (lastShotDirection === 'down') {
+        if (
+          isAttackLogical(lastShot, 'down') &&
+          !isAlreadyShot(lastShot + 10)
+        ) {
+          coordinate = lastShot + 10;
+        }
+      } else if (
+        isAttackLogical(lastShot, 'right') &&
+        !isAlreadyShot(lastShot + 1)
+      ) {
+        lastShotDirection = 'right';
+        coordinate = lastShot + 1;
+      } else if (
+        isAttackLogical(lastShot, 'left') &&
+        !isAlreadyShot(lastShot - 1)
+      ) {
+        lastShotDirection = 'left';
+        coordinate = lastShot - 1;
+      } else if (
+        isAttackLogical(firstShotHit, 'left') &&
+        !isAlreadyShot(firstShotHit - 1)
+      ) {
+        lastShotDirection = 'left';
+        coordinate = firstShotHit - 1;
+      } else if (
+        isAttackLogical(lastShot, 'up') &&
+        !isAlreadyShot(lastShot - 10)
+      ) {
+        lastShotDirection = 'up';
+        coordinate = lastShot - 10;
+      } else if (
+        isAttackLogical(lastShot, 'down') &&
+        !isAlreadyShot(lastShot + 10)
+      ) {
+        lastShotDirection = 'down';
+        coordinate = lastShot + 10;
+      }
+    }
+
+    if (itsAHit(gameBoard, coordinate) && firstShotHit === null) {
+      firstShotHit = coordinate;
+    }
+
+    if (itsAHit(gameBoard, coordinate)) {
+      lastShot = coordinate;
+    } else {
+      lastShot = null;
+    }
+
+    return coordinate;
+  };
+
+  const itsAHit = (gameBoard, index) => {
+    const boardArr = gameBoard.getBoard();
+
+    if (boardArr[index].hasShip) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const isAttackLogical = (i, directionOfAttack) => {
+    for (let j = 0; j < 10; j += 1) {
+      if (i === j * 10 - 1 && directionOfAttack === 'right') {
+        console.log('no, right');
+        return false;
+      }
+      if (i === j * 10 && directionOfAttack === 'left') {
+        console.log('no, left');
+        return false;
+      }
+      if (i === j && directionOfAttack === 'up') {
+        console.log('no, up');
+        return false;
+      }
+      if (i > 90 && directionOfAttack === 'down') {
+        console.log('no, down');
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const reset = () => {
+    shotCoordinates = [];
+    firstShotHit = null;
+    lastShot = null;
+    lastShotDirection = null;
+  };
+
+  return { attack, randomAttack, getName, reset };
 };
 
 export default Player;
